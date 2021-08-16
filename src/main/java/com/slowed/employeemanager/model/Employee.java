93a457423c62c5076ef377029a949ae91b8474ac
root-marco <rootslowed@gmail.com>
2021-08-16 10:56:35 -0300

    first commit

---
+++ src/main/java/com/slowed/employeemanager/model/Employee.java
@@ -0,0 +1,15 @@
+package com.slowed.employeemanager.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+  private Long id;
+  private String name;
+  private String email;
+  private String jobTitle;
+  private String phone;
+  private String imageUrl;
+  private String employeeCode;
+
+}
