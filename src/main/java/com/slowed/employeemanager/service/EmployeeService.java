780a42ab4391cd99274d291549a44b43207d6f31
root-marco <rootslowed@gmail.com>
2021-08-17 11:18:46 -0300

    update an employee

--- src/main/java/com/slowed/employeemanager/service/EmployeeService.java
+++ src/main/java/com/slowed/employeemanager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.slowed.employeemanager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,6 +20,14 @@ public class EmployeeService {
 
   public EmployeeModel addEmployee(EmployeeModel employee) {
     employee.setEmployeeCode(UUID.randomUUID().toString());
+    return employeeRepository.save(employee);
+  }
+
+  public List<EmployeeModel> findAllEmployees() {
+    return employeeRepository.findAll();
+  }
+
+  public EmployeeModel updateEmployee(EmployeeModel employee) {
     return employeeRepository.save(employee);
   }
 
