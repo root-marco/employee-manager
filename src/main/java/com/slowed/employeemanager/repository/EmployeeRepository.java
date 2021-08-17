0007e90d310de6490a6f9ad5e9bea1f219753749
root-marco <rootslowed@gmail.com>
2021-08-17 16:49:14 -0300

    employee repository findEmployeeById implemented

--- src/main/java/com/slowed/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/slowed/employeemanager/repository/EmployeeRepository.java
@@ -3,8 +3,12 @@ package com.slowed.employeemanager.repository;
 import com.slowed.employeemanager.model.EmployeeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<EmployeeModel, Long> {
 
   void deleteEmployeeById(Long id);
+
+  Optional<EmployeeModel> findEmployeeById(Long id);
 
 }
