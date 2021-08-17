02248d36e07872d3e3c650ce955c712eb056e6e8
root-marco <rootslowed@gmail.com>
2021-08-17 10:45:24 -0300

    employee repository initial skeleton

---
+++ src/main/java/com/slowed/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.slowed.employeemanager.repository;
+
+import com.slowed.employeemanager.model.EmployeeModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeModel, Long> {
+
+
+
+}
